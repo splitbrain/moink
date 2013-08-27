83738661fee9938491bdde1aec4400094adef779
Andreas Gohr <andi@splitbrain.org>
2013-08-27 21:52:59 +0200

    first working version

--- moink.js
+++ moink.js
@@ -2,42 +2,56 @@ var request = require('request');
 var cheerio = require('cheerio');
 var fs      = require('fs');
 var path    = require('path');
+var async   = require('async');
+var data2xml = require('data2xml')();
 
-for (var i=300; i<301; i++){
+var items = [];
+var calls = [];
 
-/*
-    var url = 'http://www.atlasobscura.com/places/'+i;
-
-    request(url, function(err, rep, body){
-        if(err) return; // ignore errors
-
-    });
-*/
-
-    fetchItem(
-        i,
-        function(id, html){
-            parseItem(id, html, function(item){
-                console.log(item);
-            });
-        }
+// add all the possible IDs to our call list
+for(var i=130; i<200; i++){
+    calls.push(
+        function(i){
+            return function(callback){
+                fetchItem(
+                        i,
+                        callback
+                );
+            };
+        }(i)
     );
 }
+// fetch all the items in parallel and come back here when done
+async.parallelLimit(calls, 5, function(err, results){
+    var xml = {
+        _attr: {
+            'version': "1.1",
+            'xmlns': "http://www.topografix.com/GPX/1/1",
+            'xmlns:xsi': "http://www.w3.org/2001/XMLSchema-instance",
+            'xmlns:gpxtpx': "http://www.garmin.com/xmlschemas/TrackPointExtension/v1",
+            'xsi:schemaLocation': "http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd"
+        },
+        wpt: results
+    }
+
+    process.stdout.write(data2xml('gpx',xml));
+});
+
 
 /**
- * parse data from the given HTML
+ * parse data from the given HTML into a waypoint structure
  */
 function parseItem(id, html, cb){
     var $ = cheerio.load(html);
     var item = {};
     item.id          = id;
-    item.url         = $('meta[property="og:url"]').attr('content').trim();
+    item.url         = $('meta[property="og:url"]').attr('content')
 
-    item.title       = $('h1.title[itemprop=name]').text().trim();
-    item.teaser      = $('h2.subtitle[itemprop=description]').text().trim();
-    item.location    = $('#item-title div.location').text().trim();
-    item.thumbnail   = $('meta[property="og:image"]').attr('content').trim();
-    item.directions  = $('#map-wrap div.directions').html().trim();
+    item.title       = $('h1.title[itemprop=name]').text();
+    item.teaser      = $('h2.subtitle[itemprop=description]').text();
+    item.location    = $('#item-title div.location').text();
+    item.thumbnail   = $('meta[property="og:image"]').attr('content');
+    item.directions  = $('#map-wrap div.directions').html();
 
     item.lat = parseFloat($('meta[itemprop=latitude]').attr('content'));
     item.lon = parseFloat($('[itemprop=longitude]').attr('content'));
@@ -45,7 +59,7 @@ function parseItem(id, html, cb){
     item.description = $('div#description')
     item.description.find('div.share-item').remove();
     item.description.find('div.edit-button').remove();
-    item.description = item.description.html().trim();
+    item.description = item.description.html();
 
     item.info = '';
     var infos = $('#item-details ul');
@@ -56,10 +70,33 @@ function parseItem(id, html, cb){
     item.tags = [];
     var tags = $('.categories a');
     for(var j=0; j<tags.length; j++){
-        item.tags.push(cheerio(tags[j]).text().trim());
+        item.tags.push(cheerio(tags[j]).text());
     }
 
-    cb(item);
+    item.html = '<p>'+item.teaser+'</p>'+
+                '<img src="'+item.thumbnail+'" align="center">'+
+                '<p>'+item.tags.join(', ')+'</p>'+
+                item.description +
+                item.info +
+                item.directions;
+
+    var xml = {
+        _attr: {
+            'lat': item.lat,
+            'lon': item.lon
+        },
+        name: item.title,
+        desc: item.html,
+        sym: 'z-favorites'
+    };
+    if(item.url){
+        xml.link = {
+            _attr: { href: item.url },
+            text: item.url
+        };
+    }
+
+    cb(null, xml);
 }
 
 /**
@@ -72,21 +109,24 @@ function fetchItem(id, cb){
     // check for cache file
     fs.exists(cache, function(exists){
         if(exists){
-            // load cache file and call callback
+            // load cache file and parse it
             fs.readFile(cache, {encoding: 'utf-8'}, function(err, data){
                 if(err) throw err;
-                cb(id, data);
+                parseItem(id, data, cb);
             });
         }else{
             // load URL
             request(url, function(err, rep, data){
-                if(err) return; // we ignore errors (404s etc)
-
-                // write cache and call callback
-                fs.writeFile(cache, data, function(err){
-                    if(err) throw err;
-                    cb(id, data);
-                });
+                if(err) throw err;
+                if(rep.statusCode == 200){
+                    // write cache and parse it
+                    fs.writeFile(cache, data, function(err){
+                        if(err) throw err;
+                        parseItem(id, data, cb);
+                    });
+                }else{
+                    cb(null); // we ignore 404s and return undefined
+                }
             });
         }
     });
